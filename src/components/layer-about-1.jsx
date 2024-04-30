79000d424b4071bc69a55f7ed963565fe2f826f0
agniain <agnianurul28@gmail.com>
2024-04-30 09:37:21 +0700

    pages

--- src/components/layer-about-1.jsx
+++ src/components/layer-about-1.jsx
@@ -1,7 +1,7 @@
 const LayerAbout1 = () => {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center p-4 md:p-10">                   
-            <div className="md:w-1/2 text-amber-900">
+        <div className="flex flex-col md:flex-row items-center justify-center">                   
+            <div className="md:w-1/2 ml-10 text-amber-900">
                 <p className="py-10 text-2xl font-bold">Transforming spaces with modern furniture that combines style and functionality.</p>
                 <p className="text-sm md:text-base">Our brand was born out of passion for creating modern furniture that elevates living spaces. With a focus on innovative design and quality craftmanship, we strive to provide furniture that not only looks great but also enhances the way you live.</p>                  
             
